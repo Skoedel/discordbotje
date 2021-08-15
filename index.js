6931fb953c869c1a4db4a439a1826a55dd51627a
Skoedel <88969519+Skoedel@users.noreply.github.com>
2021-08-15 16:57:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -107,6 +107,54 @@ client.on('message', message => {
     }
   });
  
+
+
+
+client.on('message', message => {
+
+  if (!message.guild) return;
+
+  // if the message content starts with "!ban"
+  if (message.content.startsWith('!ban')) {
+  
+    const user = message.mentions.users.first();
+
+    if (user) {
+
+      const member = message.guild.members.resolve(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Ban the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         * Read more about what ban options there are over at
+         * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
+         */
+        member
+          .ban({
+            reason: 'They were bad!',
+          })
+          .then(() => {
+
+            message.channel.send(`Successfully banned ${user.tag}`);
+          })
+          .catch(err => {
+
+            message.channel.send('I was unable to ban the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+
+        message.channel.send("That user isn't in this guild!");
+      }
+    } else {
+
+      message.channel.send("You didn't mention the user to ban!");
+    }
+  }
+});
   
      
 
