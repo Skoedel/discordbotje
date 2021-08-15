2770d075cedbb81167a7e4c9884f19cd658e21ec
Skoedel <88969519+Skoedel@users.noreply.github.com>
2021-08-16 00:16:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -213,7 +213,39 @@ client.on('message', message => {
     });
     
 
+    const ms = require ('ms');
+module.exports = {
+  name: 'mute',
+  description: "This mutes a member",
+  execute(message, args) {
+    const target = message.mentions.users.first();
+    if (target) {
 
+      let mainRole = message.guild.roles.cache.find(role => role.name === 'member');
+      let muteRole = message.guild.roles.cache.find(role => role.name === 'mute')
+      
+      let memberTarget = message.guild.members.cache.get(target.id);
+
+      if (!args[1]) {
+        memberTarget.roles.remove(mainRole.id);
+        memberTarget.roles.add(muteRole.id);
+        message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+        return
+      }
+      memberTarget.roles.remove(mainRole.id);
+      memberTarget.roles.add(muterole.id);
+      message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
+
+      setTimeout(function () {
+        memberTarget.roles.remove(muteRole.id);
+        memberTarget.roles(mainRole.id);
+        message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+      }, ms(args[1]));
+    }else {
+        message.channel.send('Cant find that member!');
+    }
+  }
+}
 
 
 client.login(process.env.token);
